95b4fb99de080a98b48015e7195b793cbfa9369c
MrTamfanX <50101968+MrTamfanX@users.noreply.github.com>
2020-05-03 12:35:47 +0700

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,12 @@
+#ini adalab untuk penginstalan bahan bahan tools akas-sqlv2
+#ada masalah ? hubungi admin@mrtamfanxcyberteam.my.id
+#WhatsApp : 0857-8041-1404 - 0859-6634-6141 - 0857-7951-5200
+pkg install perl
+pkg install clang
+pkg install openssl
+pkg install make
+cpan install Net::SSLeay::Handle
+cpan install LWP::UserAgent
+cpan install Filter::Crypto
+echo "\033[37;1m[\033[32;1m✓\033[37;1m]\033[36;1m Silahkan Di Jalankan Toolsnya Kaka ^_^"
+
